d611ea07b2115880a71e7cb2cfb71031a708fe5f
knowingtheworld13 <knowingtheworld13@gmail.com>
2025-04-16 22:20:59 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,8 @@ import pickle
 import gdown
 
 # Download the Random Forest model from Google Drive
-model_url = "https://drive.google.com/uc?id=1tCWekH26wW9cq_KtlKrI_CUsTGPl5Wso"
+model_url = "https://drive.google.com/uc?id=1p3nR1L-1lAO5mYl_1BVdTbNGNgp2aeEV"
+
 output_path = "rf_model.pkl"
 gdown.download(model_url, output_path, quiet=False)
 
